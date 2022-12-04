e3bfbe39403034a6ec88ce0db714d7b653bbf1ee
Major Tom <76742790+MajorTomDE@users.noreply.github.com>
2022-12-04 13:43:01 +0100

    Update install_wg_ui.sh

--- install_wg_ui.sh
+++ install_wg_ui.sh
@@ -10,18 +10,18 @@ echo 'Lade neue Wireguard UI Version von github'
 mkdir /opt/wireguard-ui
     arch=$(uname -m)
     if [[ $arch == x86_64* ]]; then
-        wget https://github.com/ngoduykhanh/wireguard-ui/releases/download/v0.4.0/wireguard-ui-v0.4.0-linux-amd64.tar.gz -O /opt/wireguard-ui/install.tar.gz
+        wget https://github.com/ngoduykhanh/wireguard-ui/releases/download/v0.4.0/wireguard-ui-v0.4.0-linux-amd64.tar.gz -O -q /opt/wireguard-ui/install.tar.gz
         elif [[ $arch == i*86 ]]; then
-        wget https://github.com/ngoduykhanh/wireguard-ui/releases/download/v0.4.0/wireguard-ui-v0.4.0-linux-386.tar.gz -O /opt/wireguard-ui/install.tar.gz
+        wget https://github.com/ngoduykhanh/wireguard-ui/releases/download/v0.4.0/wireguard-ui-v0.4.0-linux-386.tar.gz -O -q /opt/wireguard-ui/install.tar.gz
         elif  [[ $arch == arm* ]]; then
-        wget https://github.com/ngoduykhanh/wireguard-ui/releases/download/v0.4.0/wireguard-ui-v0.4.0-linux-arm.tar.gz -O /opt/wireguard-ui/install.tar.gz
+        wget https://github.com/ngoduykhanh/wireguard-ui/releases/download/v0.4.0/wireguard-ui-v0.4.0-linux-arm.tar.gz -O -q /opt/wireguard-ui/install.tar.gz
     fi
 echo 'Neue Wireguard UI Version von github geladen'
 
 echo 'Lade diverse helper scritps'
-wget https://raw.githubusercontent.com/MajorTomDE/wireguard/main/misc/wgui.path -O /etc/systemd/system/wgui.path
-wget https://raw.githubusercontent.com/MajorTomDE/wireguard/main/misc/wgui.service -O /etc/systemd/system/wgui.service
-wget https://raw.githubusercontent.com/MajorTomDE/wireguard/main/misc/wireguard-ui.service -O /etc/systemd/system/wireguard-ui.service
+wget https://raw.githubusercontent.com/MajorTomDE/wireguard/main/misc/wgui.path -O -q /etc/systemd/system/wgui.path
+wget https://raw.githubusercontent.com/MajorTomDE/wireguard/main/misc/wgui.service -O -q /etc/systemd/system/wgui.service
+wget https://raw.githubusercontent.com/MajorTomDE/wireguard/main/misc/wireguard-ui.service -O -q /etc/systemd/system/wireguard-ui.service
 echo 'Helper Scripts geladen'
 
 echo 'Entpacke Wireguard'
@@ -41,5 +41,5 @@ systemctl start wireguard-ui
 if systemctl is-active --quiet wireguard-ui ; then
    echo 'Installation OK'
 else
-   echo 'FEhler'
+   echo 'Fehler'
 fi
