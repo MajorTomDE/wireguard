a83579fd7234318cb18bb60aaa470895f51d679f
Major Tom <76742790+MajorTomDE@users.noreply.github.com>
2022-12-04 13:27:44 +0100

    Update install_wg_ui.sh

--- install_wg_ui.sh
+++ install_wg_ui.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-
 rm /opt/wireguard-ui/wireguard-ui
-   
+
 systemctl stop wireguard-ui
 
 mkdir /opt/wireguard-ui
@@ -14,23 +13,23 @@ mkdir /opt/wireguard-ui
         elif  [[ $arch == arm* ]]; then
         wget https://github.com/ngoduykhanh/wireguard-ui/releases/download/v0.4.0/wireguard-ui-v0.4.0-linux-arm.tar.gz -O /opt/wireguard-ui/install.tar.gz
     fi
-    wget https://raw.githubusercontent.com/MajorTomDE/wireguard_pve/main/wireguard/wgui.path -O /etc/systemd/system/wgui.path
-    wget https://raw.githubusercontent.com/MajorTomDE/wireguard_pve/main/wireguard/wgui.service -O /etc/systemd/system/wgui.service
-    wget https://raw.githubusercontent.com/MajorTomDE/wireguard_pve/main/wireguard/wireguard-ui.service -O /etc/systemd/system/wireguard-ui.service
-    
-    cd /opt/wireguard-ui
-    tar -xf install.tar.gz
-    rm install.tar.gz
 
-    systemctl daemon-reload
-    systemctl enable wgui.{path,service}
-    systemctl start wgui.{path,service}
-    systemctl enable wireguard-ui
-    systemctl start wireguard-ui
+wget https://raw.githubusercontent.com/MajorTomDE/wireguard/main/misc/wgui.path -O /etc/systemd/system/wgui.path
+wget https://raw.githubusercontent.com/MajorTomDE/wireguard/main/misc/wgui.service -O /etc/systemd/system/wgui.service
+wget https://raw.githubusercontent.com/MajorTomDE/wireguard/main/misc/wireguard-ui.service -O /etc/systemd/system/wireguard-ui.service
 
-    if systemctl is-active --quiet wireguard-ui ; then
-        echo 'Installation OK'
-        
-    else
-        echo 'FEhler'
-    fi
+cd /opt/wireguard-ui
+tar -xf install.tar.gz
+rm install.tar.gz
+
+systemctl daemon-reload
+systemctl enable wgui.{path,service}
+systemctl start wgui.{path,service}
+systemctl enable wireguard-ui
+systemctl start wireguard-ui
+
+if systemctl is-active --quiet wireguard-ui ; then
+   echo 'Installation OK'
+else
+   echo 'FEhler'
+fi
